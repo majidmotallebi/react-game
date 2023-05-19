338dd3999790766a6fbb7480730b3f96dbea92ea
majidgeo <m.motallebi68@gmail.com>
2023-05-19 21:29:19 +0330

    setue routing

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,40 @@
+import { Box,Grid, Show, GridItem, Flex } from '@chakra-ui/react'
+import { GameGrid } from '../components/GameGrid'
+import { GameHeading } from '../components/GameHeading'
+import { GenerList } from '../components/GenerList'
+import { Platform } from '../components/Platform'
+import { SortSelector } from '../components/SortSelector'
+
+export const HomePage = () => {
+  return (
+    <Grid templateAreas={{
+    base:` "main"`,
+    lg:`"asid main"`
+
+  }}
+  templateColumns={{
+    base: '1fr',
+    lg:'200px 1fr'
+  }}>
+   
+      <Show above="lg">
+        <GenerList/>
+      </Show>
+      <GridItem area="main">
+
+        <Box paddingLeft={2}>
+            <GameHeading/>
+            <Flex  marginBottom={5}>
+              <Box marginRight={2}>
+              <Platform />
+              </Box>
+              <SortSelector />
+            </Flex>
+        </Box>
+       
+        <GameGrid/>
+      </GridItem>
+
+  </Grid>
+  )
+}
