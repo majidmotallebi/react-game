028db1804609cfc3e4a1b58eec6690e9c0d610bd
majidgeo <m.motallebi68@gmail.com>
2023-05-23 00:14:21 +0330

    set navbar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,6 +1,7 @@
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 import React, { useRef } from 'react'
 import { BsSearch } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom';
 import useGameQueryStore from '../store';
 
 
@@ -9,11 +10,14 @@ import useGameQueryStore from '../store';
 export const SearchInput = () => {
 
     const ref =useRef<HTMLInputElement>(null);
-    const setSearchText= useGameQueryStore(s=>s.setSearchText)
+    const setSearchText= useGameQueryStore(s=>s.setSearchText);
+    const navigate = useNavigate();
   return (
     <form onSubmit={(event)=>{
         event.preventDefault();
-        if(ref.current) setSearchText(ref.current.value) ;
+        if(ref.current) 
+        setSearchText(ref.current.value) ;
+        navigate('/');
     }}>
     <InputGroup>
         <InputLeftElement children={<BsSearch/>} />
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,4 +1,4 @@
-import { Heading, Spinner, Text, SimpleGrid } from '@chakra-ui/react';
+import { Heading, Spinner, Text, SimpleGrid,Box, GridItem } from '@chakra-ui/react';
 import React from 'react'
 import { useParams } from 'react-router-dom';
 import DefinitionItem from '../components/DefinitionItem';
@@ -18,13 +18,21 @@ import useGame from '../hooks/useGame';
 
      if(error || !game) throw error;
   return (
-     <>
-     <Heading>{game.name}</Heading>
-    <ExpandableText>{game.description_raw}</ExpandableText>
-     <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
-      <GameScreenshots gameId={game.id}/>
-    </>
+     <SimpleGrid columns={{base:1,md:2}} spacing={5}>
+
+      <GridItem>
+               <Heading>{game.name}</Heading>
+               <ExpandableText>{game.description_raw}</ExpandableText>
+               <GameAttributes game={game} />
+      </GridItem>
+          
+      <GridItem>
+            <GameTrailer gameId={game.id} />
+            <GameScreenshots gameId={game.id}/>
+      </GridItem>
+     
+      
+    </SimpleGrid>
    
   )
 }
